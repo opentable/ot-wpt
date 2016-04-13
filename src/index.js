706ab1aa8bd222a74b6bb868a5b05289d0b1585d
vmitrevski <vmitrevski@opentable.com>
2016-04-13 10:30:58 +1000

    update to statsd func

--- src/index.js
+++ src/index.js
@@ -45,6 +45,10 @@ function notifyStatsd(data, options, done) {
     prefix: options.statsd.prefix
   })
 
+  client.socket.on("close", () => {
+    done()
+  })
+
   async.series([
     (callback) => {
       client.gauge("fv.speedindex", data.data.average.firstView.SpeedIndex, callback)
@@ -54,7 +58,6 @@ function notifyStatsd(data, options, done) {
     }
   ], () => {
     client.close()
-    done()
   })
 }
 
